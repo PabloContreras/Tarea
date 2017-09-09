1a676d3004d57abbe97c7a625e8d0cdca920f276
Pablo Contreras <pablo_contreras_1997@outlook.com>
2017-09-09 16:49:23 -0500

    Creado

---
+++ resources/views/todo/create.blade.php
@@ -0,0 +1,29 @@
+@extends('Layout.app')
+
+@section('body') 
+	<br>
+	<a href="\todo" class="btn btn-info">Back</a>
+	<div class="col-lg-4 col-lg-offset-4">
+		<h1>Create new Item</h1>
+		<form class="form-horizontal" action="/todo" method="post">
+		{{csrf_field()}}
+  		<fieldset>
+    	<div class="form-group">
+      		<div class="col-lg-10">
+        		<textarea class="form-control" name="body" rows="5" id="textArea"></textarea>
+        		<br>
+        		<button type="submit" class="btn btn-success">Submit </button>
+
+        	</div>
+    	</div>
+		</fieldset>
+   		</form>
+   		@if (count($errors)>0)
+			<div class="alert alert-danger" > 				
+   			@foreach($errors->all() as $error)
+				{{$error}}
+   			@endforeach
+   			</div>
+   		@endif
+	</div>
+@endsection 
